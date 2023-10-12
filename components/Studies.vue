cfa0d6e980179acd4efe8f32377f3eaa7f7362eb
Pedrooh <pedro.ondiviela@gmail.com>
2023-10-12 19:04:45 +0200

    build(experiences): optimizing svgs

--- components/Studies.vue
+++ components/Studies.vue
@@ -3,36 +3,47 @@
     <h2 class="cv-studies__title">Studies</h2>
     <div class="cv-studies__wrapper">
       <div class="cv-studies__section cv-studies__section--first">
-        <h3 class="cv-studies__section-title cv-studies__section-title--second">
-          Bachelor in architecture
-        </h3>
-        <p>2011-2017</p>
-        <p>Universidad de Valladolid</p>
+        <div class="cv-studies__section-text">
+          <h3 class="cv-studies__section-title cv-studies__section-title--second">
+            Bachelor in architecture
+          </h3>
+          <p class="cv-studies__section-info">2011-2017</p>
+          <p class="cv-studies__section-info">Universidad de Valladolid</p>
+        </div>
+        <img class="cv-studies__section-image" src="/assets/pedro--first.svg" alt="Pedro in architecture" />
       </div>
       <div class="cv-studies__section cv-studies__section--second">
-        <h3 class="cv-studies__section-title cv-studies__section-title--third">
-          Master's degree in Interactive Design
-        </h3>
-        <p>2017-2018</p>
-        <p>Escuela Superior de Diseño de Madrid</p>
+        <div class="cv-studies__section-text">
+          <h3 class="cv-studies__section-title cv-studies__section-title--third">
+            Master's degree in Interactive Design
+          </h3>
+          <p class="cv-studies__section-info">2017-2018</p>
+          <p class="cv-studies__section-info">Escuela Superior de Diseño de Madrid</p>
+        </div>
+        <img class="cv-studies__section-image" src="/assets/pedro--second.svg" alt="Pedro in design" />
       </div>
       <div class="cv-studies__section cv-studies__section--third">
-        <h3 class="cv-studies__section-title cv-studies__section-title--third">
-          Full stack Web Development Bootcamp
-        </h3>
-        <p>2017-2018</p>
-        <p>Escuela Superior de Diseño de Madrid</p>
-        <h3 class="cv-studies__section-title cv-studies__section-title--third">
-          5+ years of Front-end experience
-        </h3>
-        <p>2017-present</p>
+        <div class="cv-studies__section-text">
+          <h3 class="cv-studies__section-title cv-studies__section-title--third">
+            Full stack Web Development Bootcamp
+          </h3>
+          <p class="cv-studies__section-info">2017-2018</p>
+          <p class="cv-studies__section-info">Escuela Superior de Diseño de Madrid</p>
+          <h3 class="cv-studies__section-title cv-studies__section-title--third">
+            5+ years of Front-end experience
+          </h3>
+          <p class="cv-studies__section-info">2017-present</p>
+        </div>
+        <img class="cv-studies__section-image" src="/assets/pedro--third.svg" alt="Pedro as programmer" />
       </div>
     </div>
   </section>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 <style lang="scss" scoped>
   .cv-studies {
+    $component-class: &;
     background: linear-gradient(
       100deg,
       $grey 0%,
@@ -52,16 +63,22 @@
     }
     &__section {
       width: 33.33%;
-    }
-    &__section-title {
       &--first {
-        color: $black;
+        #{$component-class} {
+          &__section-title,
+          &__section-info {
+            color: $black;
+          }
+        }
       }
-      &--second {
-        color: $white;
-      }
+      &--second,
       &--third {
-        color: $white;
+        #{$component-class} {
+          &__section-title,
+          &__section-info {
+            color: $white;
+          }
+        }
       }
     }
   }
