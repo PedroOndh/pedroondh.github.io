bb6f4ec0d87e4a3f1661464104117d0fd822ea4c
Pedro Ondiviela <ondiviela@etg24.de>
2025-11-17 09:57:09 +0100

    feat(wedding): expand guest list with additional family and friends, updating names and IDs for clarity

--- utils/guests.ts
+++ utils/guests.ts
@@ -1,6 +1,6 @@
 const sarasFamily = [
   {
-    id: 'papa-y-sara',
+    id: 'papa-y-mama-sara',
     name: 'Papa y Mama',
     guests: 2,
     gender: 'male'
@@ -30,16 +30,58 @@ const sarasFamily = [
     gender: 'female'
   },
   {
-    id: 'bruno',
-    name: 'Bruno',
-    guests: 1,
+    id: 'bruno-y-aranza',
+    name: 'Bruno y Aranza',
+    guests: 2,
     gender: 'male'
   },
   {
     id: 'abuelos-sara',
-    name: 'Abuelos',
+    name: 'Miguel y Toñi',
     guests: 2,
     gender: 'male'
+  },
+  {
+    id: 'goyito',
+    name: 'Goyito y trillizos',
+    guests: 4,
+    gender: 'male'
+  },
+  {
+    id: 'marisa-y-fer',
+    name: 'Marisa y Fer',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'aranza-y-joaquin',
+    name: 'Aranza y Joaquin',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'jose-manuel-y-lorena',
+    name: 'Jose Manuel y Lorena',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'diego-y-florencia',
+    name: 'Diego y Florencia',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'pati-y-john',
+    name: 'Pati y John',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'pilita',
+    name: 'Pilita',
+    guests: 1,
+    gender: 'female'
   }
 ];
 
@@ -81,6 +123,12 @@ const sarasFriends = [
     gender: 'male'
   },
   {
+    id: 'ivan',
+    name: 'Ivan',
+    guests: 1,
+    gender: 'male'
+  },
+  {
     id: 'laura',
     name: 'Laura',
     guests: 1,
@@ -105,8 +153,80 @@ const sarasFriends = [
     gender: 'male'
   },
   {
-    id: 'laura-y-alvaro-2',
-    name: 'Laura y Alvaro',
+    id: 'paula-y-pablo',
+    name: 'Paula y Pablo',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'carlos-y-sonia',
+    name: 'Carlos y Sonia',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'lorena-y-enrique',
+    name: 'Lorena y Enrique',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'elena-y-jorge',
+    name: 'Elena y Jorge',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'mosto-y-esther',
+    name: 'Mosto y Esther',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'quintas-y-silvia',
+    name: 'Quintas y Sylvia',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'verdejo',
+    name: 'Verdejo',
+    guests: 1,
+    gender: 'male'
+  },
+  {
+    id: 'barre',
+    name: 'Barre',
+    guests: 1,
+    gender: 'male'
+  },
+  {
+    id: 'ruben-y-nerea',
+    name: 'Ruben y Nerea',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'angela-y-chavo',
+    name: 'Angela y Chavo',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'carlos-y-blanqui',
+    name: 'Carlos y Blanqui',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'carmen-y-alex',
+    name: 'Carmen y Alex',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'aida',
+    name: 'Aida',
     guests: 2,
     gender: 'male'
   }
@@ -130,6 +250,48 @@ const pedrosFamily = [
     name: 'Celia',
     guests: 1,
     gender: 'female'
+  },
+  {
+    id: 'clara-y-pepe',
+    name: 'Clara y Pepe',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'moncho-y-rosa',
+    name: 'Tío Moncho y Tía Rosa',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'paloma-y-fernando',
+    name: 'Tía Paloma y Tío Fernando',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'chus',
+    name: 'Tía Chus',
+    guests: 1,
+    gender: 'female'
+  },
+  {
+    id: 'pispa',
+    name: 'Pispa',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'abuela-fina',
+    name: 'Abuela Fina',
+    guests: 1,
+    gender: 'female'
+  },
+  {
+    id: 'alberto-e-irina',
+    name: 'Alberto e Irina',
+    guests: 2,
+    gender: 'male'
   }
 ];
 
@@ -153,6 +315,12 @@ const pedrosFriends = [
     gender: 'male'
   },
   {
+    id: 'sergio-y-paula',
+    name: 'Sergio y Paula',
+    guests: 2,
+    gender: 'male'
+  },
+  {
     id: 'oriol',
     name: 'Oriol',
     guests: 1,
@@ -164,6 +332,7 @@ const pedrosFriends = [
     guests: 2,
     gender: 'male'
   },
+  { id: 'sasi', name: 'Sasi', guests: 1, gender: 'male' },
   {
     id: 'jorge-y-may',
     name: 'Jorge y May',
@@ -181,8 +350,52 @@ const pedrosFriends = [
     name: 'Berto y Ana',
     guests: 2,
     gender: 'male'
+  },
+  {
+    id: 'clara-y-ricardo',
+    name: 'Clara y Ricardo',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'salvita',
+    name: 'Salvita',
+    guests: 1,
+    gender: 'male'
+  },
+  {
+    id: 'cesar-y-clara',
+    name: 'Cesar y Clara',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'migui',
+    name: 'Migui y Nico',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'carmen-y-miguel',
+    name: 'Carmen y Miguel',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'carmen',
+    name: 'Carmen',
+    guests: 2,
+    gender: 'male'
+  },
+  {
+    id: 'rodrigo-e-ivan',
+    name: 'Rodrigo e Ivan',
+    guests: 2,
+    gender: 'male'
   }
 ];
+
+// Jorge? y Dani Bayon? y Dani? y Luis?
 
 const guests = [...sarasFamily, ...sarasFriends, ...pedrosFamily, ...pedrosFriends];
 
